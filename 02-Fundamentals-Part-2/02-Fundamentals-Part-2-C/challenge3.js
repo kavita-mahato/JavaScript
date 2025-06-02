6a07240715aac215b35f7f43b0feba1ab52f120b
Kavita Mahato <kavitamahato0835@gmail.com>
2025-06-02 19:40:26 +0530

    Update commit

---
+++ 02-Fundamentals-Part-2/02-Fundamentals-Part-2-C/challenge3.js
@@ -0,0 +1,26 @@
+'use strict';
+/*
+const mark = {
+    fullName : "Mark Miller",
+    mass : 78,
+    height : 1.69,
+    calcBMI : function(){
+        const BMIMark = this.mass / this.height **2;
+        return BMIMark;
+    }
+}
+const john = {
+    fullName : "John Smith",
+    mass : 92,
+    height : 1.95,
+    calcBMI : function(){
+        const BMIJohn = this.mass / this.height **2;
+        return BMIJohn;
+    }
+}
+
+console.log(mark.calcBMI(),john.calcBMI());
+if (mark.calcBMI() > john.calcBMI()){
+    console.log(`${mark.fullName}'s BMI (${mark.calcBMI()}) is higher than ${john.fullName}'s BMI (${john.calcBMI()})!`);
+}
+*/
--- 02-Fundamentals-Part-2/02-Fundamentals-Part-2-C/script.js
+++ 02-Fundamentals-Part-2/02-Fundamentals-Part-2-C/script.js
@@ -1,1 +1,69 @@
 'use strict';
+
+// FUNCTION to OBJECT
+/*
+const jonas = {
+    firstName : 'Jonas',
+    lastName : 'John',
+    birthYear : 1991,
+    job : "teacher",
+    friends : ['Michael','Steven','Peter'],
+    hasLicense : false,
+
+    // calcAge : function(){
+    //     // console.log(this);
+    //     return 2025-this.birthYear;
+    // }
+
+    calcAge : function(){
+        this.age = 2025 - this.birthYear;
+        return this.age;
+    },
+
+    check : function(){
+        if(this.hasLicense === true)
+            return "he has a driver's license.";
+        else
+            return "he doesn't have drivers license.";
+    },
+
+    getSummary : function(){
+        return `${this.firstName} is a ${this.calcAge()}-year old teacher, and he has ${this.hasLicense ? 'a':'no'} driver's license.`;
+    }
+};
+
+jonas.calcAge();
+jonas.check();
+
+// CHALLENGE
+console.log(`${jonas.firstName} is a ${jonas.calcAge()}-year old teacher, and ${jonas.check()}`);
+console.log(jonas.getSummary());
+*/
+/*
+const kavita = [
+    'Kavita',
+    'Mahato',
+    'Student',
+    ['X','Y','Z']
+]
+const types =[];
+
+for (let i = 0; i < kavita.length; i++) {
+    console.log(kavita[i],typeof kavita[i]);   
+
+    // filling types array
+    // types[i] = typeof kavita[i];
+    types.push(typeof kavita[i]);
+}
+
+console.log(types);
+*/
+
+const years = [2000,2001,2002,2003,2004]
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+    ages[i] = 2025 - years[i]; 
+}
+
+console.log(ages);
