697afdb6179b412d6e7a94f14aba9a5d03d5d652
Kavita Mahato <kavitamahato0835@gmail.com>
2025-06-02 22:15:02 +0530

    Update commit

---
+++ 02-Fundamentals-Part-2/03-Developer-Skills/script.js
@@ -0,0 +1,19 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+const measureKelvin = function(){
+    const measurement = {
+        type:'temp',
+        unit:'celsius',
+        value:Number(prompt('Degree celsius')),
+    };
+    console.log(measurement);
+    console.table(measurement);
+    // console.log(measurement.value);
+    // console.warn(measurement.value);
+    // console.error(measurement.value);
+    const kelvin = measurement.value +273;
+    return kelvin;
+}
+
+console.log(measureKelvin());
