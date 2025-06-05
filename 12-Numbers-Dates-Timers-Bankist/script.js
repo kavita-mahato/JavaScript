d78fe9950bb1b1eb82071901ad9d9ff24b828cd6
Kavita Mahato <kavitamahato0835@gmail.com>
2025-06-06 01:21:46 +0530

    Math and Rounding

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -249,7 +249,7 @@ btnSort.addEventListener('click', function (e) {
 });
 
 ///////////////////////////////////////////////////   LECTURES   /////////////////////////////////////////////////
-
+/*
 console.log(23 ===23.0);
 // Base 10 - 0 to 9 
 //  Binary base  0,1
@@ -281,3 +281,39 @@ console.log(Number.isFinite(23 / 0));
 console.log(Number.isInteger(23));
 console.log(Number.isInteger(23.0));
 console.log(Number.isInteger(23 / 0));
+*/
+
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, '23', 11, 2));
+console.log(Math.max(5, 18, '23px', 11, 2));
+
+console.log(Math.min(5, 18, 23, 11, 2));
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) => Math.trunc(Math.random() * (max - min + 1)) + min;
+// const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+console.log(randomInt(10,20));
+
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+
+console.log(Math.floor(23.3));
+console.log(Math.floor('23.9'));
+
+// Rounding decimals
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(3));
+console.log((2.345).toFixed(2));
+console.log(+(2.345).toFixed(2));
+
