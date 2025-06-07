4ac1f1e11a8e0198874e7005f103d6c3d9bf302a
Kavita Mahato <kavitamahato0835@gmail.com>
2025-06-07 20:49:24 +0530

    Sticky Navigation

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -135,6 +135,21 @@ const nav = document.querySelector('.nav');
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
+// --------------------------------------------------------------
+
+// Sticky navigation: Intersection Observer API
+
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+
+window.addEventListener('scroll',function(){
+  console.log(window.scrollY);
+  if(window.scrollY > initialCoords.top) nav.classList.add('sticky');
+});
+
+
+// --------------------------------------------------------------
+
 /*
 nav.addEventListener("mouseover", function (e) {
   if (e.target.classList.contains("nav__link")) {
