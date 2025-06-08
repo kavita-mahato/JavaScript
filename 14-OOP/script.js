187dba1e521928da76c27a2066768e5a71ec999e
Kavita Mahato <kavitamahato0835@gmail.com>
2025-06-08 17:58:53 +0530

    Setters & Getters

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -91,11 +91,27 @@ class PersonCl {
   greet() {
     console.log(`Hey ${this.fullName}`);
   }
+
+  get age() {
+    return 2025 - this.birthYear;
+  }
+  // Set a property that already exists
+  set fullName(name) {
+    console.log(name);
+    // _fullname so that ano conflict occurs with fullname in the constructor method
+    if (name.includes(' ')) this._fullName = name;       
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
 }
 
 const kavita = new PersonCl('Kavita Mahato', 2005);
 console.log(kavita);
 kavita.calcAge();
+console.log(kavita.age);
 console.log(kavita.__proto__ === PersonCl.prototype);
 
 // PersonCl.prototype.greet = function () {
@@ -107,3 +123,25 @@ kavita.greet();
 // 1. Classes are NOT hoisted
 // 2. Classes are first-class citizens
 // 3. Classes are executed in strict mode
+
+const antara = new PersonCl('Antara', 2000);
+
+// -------------------------------------------------------------------
+// Setters and Getters
+const account = {
+  owner: 'Kavita',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 500;
+console.log(account.movements);
