1a53e41e37fdabd8e7dd34381ff15c5901f5e484
Kavita Mahato <kavitamahato0835@gmail.com>
2025-06-08 16:57:50 +0530

    Prototypal Inheritance in built in objects

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -43,4 +43,24 @@ Person.prototype.species = 'Homo Sapiens';
 console.log(kavita.species, matilda.species);
 console.log(kavita.hasOwnProperty('species'));
 
-// console.log(Person.prototype.constructor);
+
+console.log(kavita.__proto__);
+// Object.prototype (top of the prototype chain)
+console.log(kavita.__proto__.__proto__);
+console.log(kavita.__proto__.__proto__.__proto__);
+console.log(Person.prototype.constructor);
+console.dir(Person.prototype.constructor);
+
+const arr = [3,6,4,5,6,9,3];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function(){
+    return [...new Set(this)];
+}
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x+1);
