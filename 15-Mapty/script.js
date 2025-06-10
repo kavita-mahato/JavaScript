202518289a817fb85bfd9da2dc05152a75eb3670
Kavita Mahato <kavitamahato0835@gmail.com>
2025-06-10 05:48:20 +0530

    Managing Workout data creating classes

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -13,6 +13,47 @@ const inputElevation = document.querySelector(".form__input--elevation");
 
 let map, mapEvent;
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+  constructor(coords, distance, duration){
+    // this.date = ...
+    // this.id = ...
+    this.coords = coords;
+    this.distance = distance;  // in km
+    this.duration = duration;  // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence){
+    super(coords,distance,duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+  calcPace (){
+    // min/km
+    this.pace = this.duration/this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout{
+  constructor(coords, distance, duration, elevation){
+    super(coords,distance,duration);
+    this.elevation = elevation;
+  }
+  calcSpeed(){
+    // km/h
+    this.speed = this.distance / (this.duration/60);
+    return this.speed;
+  }
+}
+
+const run1 = new Running([39, -12], 5.2, 24, 178);
+const cycling1 = new Cycling([39, -12], 27, 523);
+console.log(run1, cycling1);
+
 class App {
   #map;
   #mapEvent;
@@ -34,7 +75,7 @@ class App {
   _loadmap(position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
-    console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+    // console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
 
     const coords = [latitude, longitude];
 
