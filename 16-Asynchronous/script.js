78d47de1549ddff6b67934e33061c33e7a933bee
Kavita Mahato <kavitamahato0835@gmail.com>
2025-06-11 18:17:55 +0530

    Event loop

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -182,6 +182,7 @@ getCountryData('australia');
 */
 
 // _____________________________Challenge #1 ________________________
+/*
 const whereAmI = function (lat, lng) {
   fetch(
     `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=json`
@@ -209,3 +210,16 @@ const whereAmI = function (lat, lng) {
 whereAmI(52.508, 13.381);
 // whereAmI(19.037, 72.873);
 // whereAmI(-33.933, 18.474);
+*/
+// ___________________________________________________________________
+// The Event Loop in Practice
+console.log('Test start');
+setTimeout(() => console.log('0 sec timer'), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log(res);
+});
+
+console.log('Test end');
