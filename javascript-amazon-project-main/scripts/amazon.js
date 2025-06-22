6c8795af426ce60361426715525f793b6c55c7cf
Kavita Mahato <kavitamahato0835@gmail.com>
2025-06-23 04:24:43 +0530

    Amazon Project Completed

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -15,7 +15,7 @@ products.forEach((product) => {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src=images/ratings/rating-${product.rating.stars*10}.png>
+              src=images/ratings/rating-${product.rating.stars * 10}.png>
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
@@ -47,7 +47,8 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart">
+          <button class="add-to-cart-button 
+          button-primary js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>`;
@@ -59,6 +60,37 @@ document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
-    console.log("Added products");
+    const productId = button.dataset.productId;
+
+    // Get the selected quantity from the dropdown
+    const productContainer = button.closest(".product-container");
+    const quantitySelect = productContainer.querySelector("select");
+    const selectedQuantity = parseInt(quantitySelect.value);
+
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += selectedQuantity;
+    } else {
+      cart.push({
+        productId,
+        quantity: selectedQuantity,
+      });
+    }
+
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+
+    console.log(cartQuantity);
+    console.log(cart);
   });
 });
